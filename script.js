e0b6e27c1cf1179cec5900db31cd4e3f5f25fa5f
Atif Iqbal <39873557+atif-dev@users.noreply.github.com>
2018-09-25 18:58:20 +0500

    background color generator

---
+++ script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+
+var button = document.querySelector("button");
+
+button.addEventListener("click" ,function(){
+document.querySelector(".color1").setAttribute("value","#824b00");
+document.querySelector(".color2").setAttribute("value","#4b0082");
+
+
+})
